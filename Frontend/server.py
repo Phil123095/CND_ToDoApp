329a6813dcd6a659d78f097bea68d5fb94061a06
cococokoko <coco.koban@student.ie.edu>
2022-06-12 17:02:35 +0200

    frontend

---
+++ Frontend/server.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime, timedelta, timezone
+import urllib.request, json, requests
+
+
+todo = Flask(__name__)
+todo.config["SECRET_KEY"] = "group1lovescoding"
+
+@todo.route("/",  methods=["GET","POST"])
+def index():
+    response = urllib.request.urlopen("https://todoapp-backend-final-7qlre2lo3a-oa.a.run.app/list-all")
+    data = response.read()
+    dict = json.loads(data)
+    edit = False
+    return render_template("base.html", todos=dict["list"], edit=edit)
+
+@todo.route("/save", methods=["POST"])
+def save():
+    if request.method == 'POST':
+        title = request.form["title"]
+        content = request.form["todo"]
+        msg = {'title': title, 'content': content}
+        res = requests.post('https://todoapp-backend-final-7qlre2lo3a-oa.a.run.app/create-todo', json=msg)
+        return redirect(url_for("index"))
+
+@todo.route("/update/<todo_id>/<title>/<content>", methods=['GET', "POST"])
+def update(todo_id, title, content):
+    if request.method == 'POST':
+        id = todo_id
+        title = request.form["title"]
+        content = request.form["content"]
+        msg = {'ID': id, 'title': title, 'content': content}
+        res = requests.post('https://todoapp-backend-final-7qlre2lo3a-oa.a.run.app/update-todo', json=msg)
+        return redirect(url_for("index"))
+    else:
+        return render_template("update.html", todo_id=todo_id, title=title, content=content)
+
+@todo.route("/delete/<todo_id>", methods=['GET', "POST"])
+def delete(todo_id):
+    #last_updated = datetime.now(timezone.utc) --> when i update the created time also changes
+    msg = {'ID': todo_id}
+    res = requests.post('https://todoapp-backend-final-7qlre2lo3a-oa.a.run.app/delete-todo', json=msg)
+    return redirect(url_for("index"))
+
+
+todo.run(debug=True)
